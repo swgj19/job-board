b6481d4845d9f45cf89debd7e50a05e878c2a021
“Stephen <swgj19@yahoo.com>
2023-11-25 12:15:33 -0700

    Setup register page & test form as event target

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,51 @@
+import { useState, useEffect } from 'react';
+import { Logo } from '../components';
+import Wrapper from '../assets/wrappers/RegisterPage.js';
+
+const initialState = {
+	name: '',
+	email: '',
+	password: '',
+	isMember: true,
+};
+
 const Register = () => {
-	return <div>Register</div>;
+	const [values, setValues] = useState(initialState);
+
+	const handleChange = (e) => {
+		console.log(e.target);
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(e.target);
+	};
+
+	return (
+		<Wrapper className='full-page'>
+			<form className='form' onSubmit={handleSubmit}>
+				<Logo />
+				<h3>Login</h3>
+				{/* name field */}
+				<div className='form-row'>
+					<label forhtml='name' className='form-label'>
+						Name
+					</label>
+					<input
+						type='text'
+						name='name'
+						placeholder='name'
+						value={values.name}
+						onChange={handleChange}
+						className='form-input'
+					/>
+				</div>
+				<button type='submit' className='btn btn-block'>
+					Submit
+				</button>
+			</form>
+		</Wrapper>
+	);
 };
 
 export default Register;
