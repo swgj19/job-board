2774ec7264962174f55f65d2b7f9fc482362d329
“Stephen <swgj19@yahoo.com>
2023-11-25 15:57:58 -0700

    Handle change state & empty values

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -13,12 +13,18 @@ const Register = () => {
 	const [values, setValues] = useState(initialState);
 
 	const handleChange = (e) => {
-		console.log(e.target);
+		const name = e.target.name;
+		const value = e.target.value;
+		console.log(name, value);
+		setValues({ ...values, [name]: value });
 	};
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		console.log(e.target);
+		const { name, email, password, isMember } = values;
+		if (!email || !password || (!isMember && !name)) {
+			console.log('Please fill out all fields.');
+		}
 	};
 
 	const toggleMembers = (e) => {
