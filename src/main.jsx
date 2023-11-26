184c236787bba1c784d22b113fb194f80b7036b6
“Stephen <swgj19@yahoo.com>
2023-11-25 19:36:28 -0700

    Redux tool-kit installed & userSlice setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import 'normalize.css';
 import './index.css';
+import { store } from './store';
+import { Provider } from 'react-redux';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
-root.render(<App />);
+root.render(
+	<Provider store={store}>
+		<App />
+	</Provider>
+);
