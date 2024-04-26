7e0a2936d3eea6c429ca297f825f02d3603b4f53
“Stephen <swgj19@yahoo.com>
2024-04-25 19:33:46 -0600

    Smallsidebar toggle

--- src/components/SmallSidebar.jsx
+++ src/components/SmallSidebar.jsx
@@ -6,11 +6,24 @@ import { useSelector, useDispatch } from 'react-redux';
 import { toggleSidebar } from '../features/user/userSlice';
 
 const SmallSideBar = () => {
+	/* useSelector((store) => store.user)
+	You can think of a selector function in Redux as using dot notation to access a specific property of an object—in this case, the Redux store's state object. The selector function essentially selects or retrieves a particular part of the state. */
+
+	const { isSidebarOpen } = useSelector((store) => store.user);
+	const dispatch = useDispatch();
+	const toggle = () => {
+		dispatch(toggleSidebar());
+	};
+
 	return (
 		<Wrapper>
-			<div className='sidebar-container show-sidebar'>
+			<div
+				className={
+					isSidebarOpen ? 'sidebar-container show-sidebar' : 'sidebar-container'
+				}
+			>
 				<div className='content'>
-					<button className='close-btn' onClick={() => console.log('toggle')}>
+					<button className='close-btn' onClick={toggle}>
 						<FaTimes />
 					</button>
 					<header>
