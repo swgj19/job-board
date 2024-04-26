83b91298766d762d5446ad8033985294557b385b
“Stephen <swgj19@yahoo.com>
2024-04-25 23:49:17 -0600

    Big sidebar component

--- src/components/BigSidebar.jsx
+++ src/components/BigSidebar.jsx
@@ -1,7 +1,26 @@
+import NavLinks from './NavLinks';
+import Logo from '../components/Logo';
 import Wrapper from '../assets/wrappers/BigSidebar';
+import { useSelector } from 'react-redux';
 
 const BigSideBar = () => {
-	return <Wrapper>BigSidebar</Wrapper>;
+	const { isSidebarOpen } = useSelector((store) => store.user);
+	return (
+		<Wrapper>
+			<div
+				className={
+					isSidebarOpen ? 'sidebar-container' : 'sidebar-container show-sidebar'
+				}
+			>
+				<div className='content'>
+					<header>
+						<Logo />
+					</header>
+					<NavLinks />
+				</div>
+			</div>
+		</Wrapper>
+	);
 };
 
 export default BigSideBar;
