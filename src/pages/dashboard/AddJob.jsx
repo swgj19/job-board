e910460cb8abe5541bcf5e232fddd94e542bfbbb
“Stephen <swgj19@yahoo.com>
2024-04-27 16:11:56 -0600

    Add job structure

--- src/pages/dashboard/AddJob.jsx
+++ src/pages/dashboard/AddJob.jsx
@@ -16,26 +16,25 @@ const AddJob = () => {
 		isEditing,
 		editJobId,
 	} = useSelector((store) => store.job);
-	const dispatch = useDispatch();
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-
 		if (!position || !company || !jobLocation) {
-			toast.error('Please Fill Out All Fields');
+			toast.error('Please fill out all fields');
 			return;
 		}
 	};
+
 	const handleJobInput = (e) => {
 		const name = e.target.name;
 		const value = e.target.value;
+		console.log(name, value);
 	};
 
 	return (
 		<Wrapper>
 			<form className='form'>
 				<h3>{isEditing ? 'edit job' : 'add job'}</h3>
-
 				<div className='form-center'>
 					{/* position */}
 					<FormRow
@@ -44,26 +43,21 @@ const AddJob = () => {
 						value={position}
 						handleChange={handleJobInput}
 					/>
-					{/* company */}
+					{/* position */}
 					<FormRow
 						type='text'
 						name='company'
 						value={company}
 						handleChange={handleJobInput}
 					/>
-					{/* location */}
+					{/* position */}
 					<FormRow
 						type='text'
-						labelText='job location'
 						name='jobLocation'
+						labelText='job location'
 						value={jobLocation}
 						handleChange={handleJobInput}
 					/>
-					{/* job status */}
-
-					{/* job type */}
-
-					{/* btn container */}
 					<div className='btn-container'>
 						<button
 							type='button'
@@ -74,7 +68,7 @@ const AddJob = () => {
 						</button>
 						<button
 							type='submit'
-							className='btn btn-block submit-btn'
+							className='btn btn-block clear-btn'
 							onClick={handleSubmit}
 							disabled={isLoading}
 						>
@@ -88,3 +82,89 @@ const AddJob = () => {
 };
 
 export default AddJob;
+
+// const AddJob = () => {
+// 	const {
+// 		isLoading,
+// 		position,
+// 		company,
+// 		jobLocation,
+// 		jobType,
+// 		jobTypeOptions,
+// 		status,
+// 		statusOptions,
+// 		isEditing,
+// 		editJobId,
+// 	} = useSelector((store) => store.job);
+// 	const dispatch = useDispatch();
+
+// 	const handleSubmit = (e) => {
+// 		e.preventDefault();
+
+// 		if (!position || !company || !jobLocation) {
+// 			toast.error('Please Fill Out All Fields');
+// 			return;
+// 		}
+// 	};
+// 	const handleJobInput = (e) => {
+// 		const name = e.target.name;
+// 		const value = e.target.value;
+// 	};
+
+// 	return (
+// 		<Wrapper>
+// 			<form className='form'>
+// 				<h3>{isEditing ? 'edit job' : 'add job'}</h3>
+
+// 				<div className='form-center'>
+// 					{/* position */}
+// 					<FormRow
+// 						type='text'
+// 						name='position'
+// 						value={position}
+// 						handleChange={handleJobInput}
+// 					/>
+// 					{/* company */}
+// 					<FormRow
+// 						type='text'
+// 						name='company'
+// 						value={company}
+// 						handleChange={handleJobInput}
+// 					/>
+// 					{/* location */}
+// 					<FormRow
+// 						type='text'
+// 						labelText='job location'
+// 						name='jobLocation'
+// 						value={jobLocation}
+// 						handleChange={handleJobInput}
+// 					/>
+// 					{/* job status */}
+
+// 					{/* job type */}
+
+// 					{/* btn container */}
+// 					<div className='btn-container'>
+// 						<button
+// 							type='button'
+// 							className='btn btn-block clear-btn'
+// 							onClick={() => console.log('clear values')}
+// 						>
+// 							clear
+// 						</button>
+// 						<button
+// 							type='submit'
+// 							className='btn btn-block submit-btn'
+// 							onClick={handleSubmit}
+// 							disabled={isLoading}
+// 						>
+// 							submit
+// 						</button>
+// 					</div>
+// 				</div>
+// 			</form>
+// 		</Wrapper>
+// 	);
+// };
+
+// export default AddJob;
